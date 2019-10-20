b03321d7f579464829862e2f37f85336d9b35734
Nguyen Tri Minh <minh.nguyentri.95@gmail.com>
2019-10-20 21:49:50 +0900

    complete upload image api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -37,3 +37,11 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serialier for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
